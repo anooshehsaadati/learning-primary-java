15a8f20324116b8db9526cdaf2d188002aa35a78
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-17 20:13:39 +0330

    First new project on java in IntelliJ IDEA

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String text = "In the name of God";
+        System.out.println(text);
+    }
+}
