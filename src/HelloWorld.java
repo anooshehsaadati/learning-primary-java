f1a9f6687ffec8558c677ccbc5fa305de126b6e7
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-17 21:09:30 +0330

    javadoc comment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld {
     /*
      * This is a multi-line comment.
      */
+
+    /**
+     * This is a javadoc comment.
+     */
     public static void main(String[] args) { // This is main method
         String text = "In the name of God";
         System.out.println(text);
