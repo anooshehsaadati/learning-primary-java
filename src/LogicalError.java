b528cbc12e73805f08e73eb86b5289c0f1c739c2
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-17 20:47:01 +0330

    solution is () or third variable

--- src/LogicalError.java
+++ src/LogicalError.java
@@ -2,6 +2,6 @@ public class LogicalError {
     public static void main(String[] args) {
         int numA = 4;
         int numB = 20;
-        System.out.println("The sum is " + numA + numB);
+        System.out.println("The sum is " + (numA + numB));
     }
 }
