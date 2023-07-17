a917bbaf3f6e9a92481d9d46db3c00ba8ee19060
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-17 20:44:16 +0330

    Test for logical error in java - the result wrong because 2 number is first sum then add to String

---
+++ src/LogicalError.java
@@ -0,0 +1,7 @@
+public class LogicalError {
+    public static void main(String[] args) {
+        int numA = 4;
+        int numB = 20;
+        System.out.println("The sum is " + numA + numB);
+    }
+}
