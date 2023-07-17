5d218339f561541dbee3292f4af54f6c4e8ada67
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-17 20:59:40 +0330

    multi-line comment and end-line comment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
-    /**
-     * @param args
+    /*
+     * This is a multi-line comment.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) { // This is main method
         String text = "In the name of God";
         System.out.println(text);
     }
