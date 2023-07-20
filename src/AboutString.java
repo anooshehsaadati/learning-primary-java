0473f2f75ab7cce78062907dadfb731a396a39d4
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-20 14:06:07 +0330

    intro of String Class

---
+++ src/AboutString.java
@@ -0,0 +1,23 @@
+public class AboutString {
+    public static void main(String[] args) {
+        String myString = "My Name is Anushe";
+        System.out.println(myString);
+
+        System.out.println("My Name is Anushe");
+
+        System.out.print("My First Name is Anushe, ");
+        System.out.print("My Last Name is Saadati");
+
+        System.out.print("\t \n");
+
+        System.out.println("I Like the \"Java\" Programming Language");
+
+        System.out.println("I like the \"Java\" Programming Language." +
+                "This language is used to create android apps.");
+
+        int studentNumber = 20;
+        int teacherNumber = 6;
+        System.out.println("The number of student is " + studentNumber);
+        System.out.println("The number of teacher is " + teacherNumber);
+    }
+}
