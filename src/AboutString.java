0e1cc4e10077342e7bd2837e96460005d51c9afb
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-20 14:49:11 +0330

    some method of String Class

--- src/AboutString.java
+++ src/AboutString.java
@@ -19,5 +19,17 @@ public class AboutString {
         int teacherNumber = 6;
         System.out.println("The number of student is " + studentNumber);
         System.out.println("The number of teacher is " + teacherNumber);
+
+        // Methods
+        String firstName = "Anushe";
+        String lastName = "Saadati";
+        System.out.println("My First Name is " + firstName + " My Last Name is " + lastName
+                + " And my firstname is equal to my lastname? "+ firstName.equals(lastName));
+
+        String sentence = "This is a short sentence.";
+        System.out.println("the length of sentence is " + sentence.length());
+
+        System.out.println("lowercase of my name is " + firstName.toLowerCase() + " " + lastName.toLowerCase());
+        System.out.println("uppercase of my name is " + firstName.toUpperCase() + " " + lastName.toUpperCase());
     }
 }
