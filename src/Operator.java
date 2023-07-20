3f65954ef435bbb47834b146cc598b0e383532bd
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-20 15:33:25 +0330

    My Weight on different Planet

---
+++ src/Operator.java
@@ -0,0 +1,18 @@
+public class Operator {
+    public static void main(String[] args) {
+        // My Weight on Earth
+        System.out.print("My Weight on Earth ");
+        double myWeightOnEarth = 75.5;
+        System.out.println(myWeightOnEarth);
+
+        // My Weight on Jupiter
+        System.out.print("My Weight on Jupiter ");
+        double myWeightOnJupiter = 75.5 * 2.52;
+        System.out.println(myWeightOnJupiter);
+
+        // My Weight on Saturn
+        System.out.print("My Weight on Saturn ");
+        double myWeightOnSaturn = 75.5 * 1.06;
+        System.out.println(myWeightOnSaturn);
+    }
+}
