96ba772deebef1af0fab7f6be3205fa384aded1c
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-23 21:28:25 +0330

    use break and continue in loops

--- src/WhileIntro.java
+++ src/WhileIntro.java
@@ -1,6 +1,7 @@
 public class WhileIntro {
     public static void main(String[] args) {
         // while
+        System.out.println("Intro of While: ");
         int number = 1;
         while (number <= 10) {
             System.out.println("My Number is " + number);
@@ -8,6 +9,7 @@ public class WhileIntro {
         }
 
         // do ... while
+        System.out.println("\nIntro of do...while: ");
         int count = 1;
         do{
             System.out.println("My Count is " + count);
@@ -15,7 +17,7 @@ public class WhileIntro {
         } while (count <= 10);
 
         // diff of while and do...while
-        System.out.println("Diff of While and do...while: ");
+        System.out.println("\nDiff of While and do...while: ");
         int number_diff = 12;
         while (number_diff <= 10) {
             System.out.println("My Number is " + number_diff);
@@ -26,5 +28,19 @@ public class WhileIntro {
             System.out.println("My Count is " + count_diff);
             count_diff++;
         } while (count_diff <= 10);
+
+        // break and continue
+        System.out.println("\nBreak and Continue: ");
+        int num = 0;
+        while (num < 1000) {
+            num += 100;
+            if (num == 300) {
+                continue; // skip this iteration
+            }
+            System.out.println("My Number Is: " + num);
+            if (num == 700) {
+                break; // finish all iteration
+            }
+        }
     }
 }
