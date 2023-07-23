94bedeaa13a7f0db805a5a21c5fb4c3d7b28f4f6
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-23 21:17:44 +0330

    Introduction about While loop

---
+++ src/WhileIntro.java
@@ -0,0 +1,9 @@
+public class WhileIntro {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number <= 10) {
+            System.out.println("My Number is " + number);
+            number++;
+        }
+    }
+}
