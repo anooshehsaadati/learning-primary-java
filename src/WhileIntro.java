3cba5a6647390ecf7b28f0ca4c7a1807c2944e50
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-23 21:22:49 +0330

    do...while and different between while and do...while

--- src/WhileIntro.java
+++ src/WhileIntro.java
@@ -1,9 +1,30 @@
 public class WhileIntro {
     public static void main(String[] args) {
+        // while
         int number = 1;
         while (number <= 10) {
             System.out.println("My Number is " + number);
             number++;
         }
+
+        // do ... while
+        int count = 1;
+        do{
+            System.out.println("My Count is " + count);
+            count++;
+        } while (count <= 10);
+
+        // diff of while and do...while
+        System.out.println("Diff of While and do...while: ");
+        int number_diff = 12;
+        while (number_diff <= 10) {
+            System.out.println("My Number is " + number_diff);
+            number_diff++;
+        }
+        int count_diff = 12;
+        do{
+            System.out.println("My Count is " + count_diff);
+            count_diff++;
+        } while (count_diff <= 10);
     }
 }
