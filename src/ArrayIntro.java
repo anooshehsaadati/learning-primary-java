37ee0e2f732884febed1461e172eb08dd89cd020
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-25 17:59:24 +0330

    Intro Array in one dimensional array

---
+++ src/ArrayIntro.java
@@ -0,0 +1,15 @@
+public class ArrayIntro {
+    public static void main(String[] args) {
+        String[] friendsName = new String[3];
+        int[] friendsNumber = new int[3];
+        friendsName[0] = "MohamadReza";
+        friendsName[1] = "Ali";
+        friendsName[2] = "Sepehr";
+        friendsNumber[0] = 910123124;
+        friendsNumber[1] = 920341542;
+        friendsNumber[2] = 981634891;
+        System.out.println("Name of my first friend is " + friendsName[0] + " and number is " + friendsNumber[0]);
+        System.out.println("Name of my second friend is " + friendsName[1] + " and number is " + friendsNumber[1]);
+        System.out.println("Name of my third friend is " + friendsName[2] + " and number is " + friendsNumber[2]);
+    }
+}
