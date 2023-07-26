02451e75bfd7e1c870f93fdba0f1b782a3431d7d
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-26 10:23:55 +0330

    Introduction about methods

---
+++ src/OOP/Method/MethodIntro.java
@@ -0,0 +1,9 @@
+package OOP.Method;
+
+public class MethodIntro {
+    int price = 1000;
+    String laptopName = "ASUS ZenBook";
+    public void showData() {
+        System.out.println("Name of laptop is " + laptopName + " and its price is " + price + "$");
+    }
+}
---
+++ src/OOP/Method/MethodMain.java
@@ -0,0 +1,8 @@
+package OOP.Method;
+
+public class MethodMain {
+    public static void main(String[] args) {
+        MethodIntro methodIntro = new MethodIntro();
+        methodIntro.showData();
+    }
+}
