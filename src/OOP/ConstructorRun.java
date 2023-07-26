11883ccd2d64f0bff434d19e730aa65108744cf7
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-26 11:35:50 +0330

    Introduction of Constructor

---
+++ src/OOP/ConstructorRun.java
@@ -0,0 +1,8 @@
+package OOP;
+
+public class ConstructorRun {
+    public static void main(String[] args) {
+        ConstructorIntro constructorIntro = new ConstructorIntro("Anushe", 24, false);
+        constructorIntro.displayDetails();
+    }
+}
