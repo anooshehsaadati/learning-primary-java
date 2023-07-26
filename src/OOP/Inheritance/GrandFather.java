60e14fc7deeeb45498f25a11d0c533f22a25e7f0
Anoosheh Saadati <anoosheh.saadati.as@gmail.com>
2023-07-26 09:55:59 +0330

    Inheritance of children - GrandFather, Father, Son

---
+++ src/OOP/Inheritance/GrandFather.java
@@ -0,0 +1,38 @@
+package OOP.Inheritance;
+
+public class GrandFather {
+    public void showHeight() {
+        String height = "Short";
+        System.out.println("Height: " + height);
+    }
+
+    public void showSkinColor() {
+        String skinColor = "Bright";
+        System.out.println("Skin Color: " + skinColor);
+    }
+
+    public void showBaldness() {
+        String baldness = "Bald";
+        System.out.println("Baldness: " + baldness);
+    }
+
+    public void showBehavior() {
+        String behavior = "Angry";
+        System.out.println("Behavior: " + behavior);
+    }
+
+    public void showCreativity() {
+        String creativity = "Very Creative";
+        System.out.println("Creativity: " + creativity);
+    }
+
+    public void showActivity() {
+        String activity = "Active";
+        System.out.println("Activity: " + activity);
+    }
+
+    public void showNationality() {
+        String nationality = "Iranian";
+        System.out.println("Nationality: " + nationality);
+    }
+}
